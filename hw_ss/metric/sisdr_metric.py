55d654fba4b1037e03e8db470731ec36f509d832
Kirill Korolev <kakorolev@edu.hse.ru>
2023-11-11 23:09:11 +0000

    ablation sisdr

--- hw_ss/metric/sisdr_metric.py
+++ hw_ss/metric/sisdr_metric.py
@@ -8,10 +8,19 @@ from hw_ss.base.base_metric import BaseMetric
 
 
 class SISDRMetric(BaseMetric):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, pred="short", *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.si_sdr = ScaleInvariantSignalDistortionRatio()
+        self.pred = pred
 
-    def __call__(self, mix_short: Tensor, target: Tensor, **kwargs):
+
+    def __call__(self, mix_short: Tensor, mix_middle: Tensor, mix_long: Tensor, target: Tensor, **kwargs):
         metric = self.si_sdr.to(mix_short.device)
-        return metric(mix_short, target).item()
+        if self.pred == "short":
+            return metric(mix_short, target).item()
+        if self.pred == "middle":
+            return metric(mix_middle, target).item()
+        if self.pred == "long":
+            return metric(mix_long, target).item()
+        if self.pred == "all":
+            return metric((mix_short + mix_middle + mix_long) / 3, target).item()
